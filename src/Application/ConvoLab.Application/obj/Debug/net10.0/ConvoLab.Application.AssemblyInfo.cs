15aa64dc811db22e750543f10d3e1c221561c8b8
Kevin <kevinkutoane@ymail.com>
2026-07-09 13:55:01 +0200

    Update assembly informational versions and project directory paths for ConvoLab.Application and ConvoLab.Domain

--- src/Application/ConvoLab.Application/obj/Debug/net10.0/ConvoLab.Application.AssemblyInfo.cs
+++ src/Application/ConvoLab.Application/obj/Debug/net10.0/ConvoLab.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConvoLab.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ba9cb1e1eeb2e1845270656558b8a0889d43db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e7cdbf1029a734298fd2b46e7fc53e330d605e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConvoLab.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConvoLab.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
